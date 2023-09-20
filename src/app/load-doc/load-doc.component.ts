7b2a6a1d4df4a3dfdf5fd1fb3e1f535d8fd56232
arunrajkhumar <arkhumar@vendasta.com>
2023-09-20 18:21:45 +0530

    final

--- src/app/load-doc/load-doc.component.ts
+++ src/app/load-doc/load-doc.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-load-doc',
   templateUrl: './load-doc.component.html',
   styleUrls: ['./load-doc.component.scss']
 })
-export class LoadDocComponent {
+export class LoadDocComponent implements OnInit {
+  public showContent: boolean = false;
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+
+    setTimeout(()=>this.showContent=true, 1000);
+    
+    this.apiService.generateYaml().subscribe((response:any) => {
+
+      console.log("DONE with generation!!")
+      
+    });
+  }
+
+  ngOnDestroy(): void {
+  }
+
 }
